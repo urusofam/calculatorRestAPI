1af806797bc6d9aa694abcf4886dc87eed64e5c6
urusofam <urusofam@gmail.com>
2025-05-28 12:54:35 +0300

    add logger

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/urusofam/calculatorRestAPI/config"
+	"log/slog"
+	"os"
 )
 
 func main() {
@@ -9,5 +11,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	_ = config.Server
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	logger.Info("config file loaded", slog.String("cfg", config.Server.Host))
 }
