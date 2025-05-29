78cb85a6c0f939a6e793442992d68cd3f6c256dd
urusofam <urusofam@gmail.com>
2025-05-29 23:16:27 +0300

    add gin router

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/urusofam/calculatorRestAPI/config"
 	"log/slog"
+	"net/http"
 	"os"
 )
 
@@ -14,4 +17,12 @@ func main() {
 
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	logger.Info("config file loaded", slog.String("cfg", config.Server.Host))
+
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Привет, Gin!")
+	})
+
+	router.Run(fmt.Sprintf("%s:%d", config.Server.Host, config.Server.Port))
 }
